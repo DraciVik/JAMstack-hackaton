7fddf1e9d679059e31e139c122874f1d3ef57d15
DraciVik <viktor.stojanov90@gmail.com>
2018-08-30 23:09:44 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is done so I can get a ticket to JAMstack hackaton</p>
+    <p>Now gimme all the lovin'</p>
+    <Link to="/page-2/">To the future!</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Gimme a ticket please</h1>
+    <p>Preety please</p>
+    <Link to="/">We have to go back Marty!</Link>
+  </div>
+)
+
+export default SecondPage
